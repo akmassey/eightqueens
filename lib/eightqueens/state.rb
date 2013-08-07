81b12df1a8d6421a1e2720ca40b42af6d7c93449
Aaron Massey <akmassey@sixlines.org>
2013-08-07 19:21:32 -0400

    added diagonal attacks

--- lib/eightqueens/state.rb
+++ lib/eightqueens/state.rb
@@ -1,3 +1,4 @@
+require 'logger'
 require 'pry'
 
 module EightQueens
@@ -5,9 +6,14 @@ module EightQueens
     def initialize(queen_positions)
       raise "Invalid queen positions" unless valid_queen_positions(queen_positions)
       @queens = queen_positions
+
+      @log = Logger.new(STDOUT)
+      @log.level = Logger::WARN
     end
 
     # returns the number of queens horizontally attacking one another
+    # TODO: is this double counting every attack because we're using a cross
+    # product?
     def horizontal_attacks
       attacks = 0
 
@@ -16,6 +22,27 @@ module EightQueens
       end
 
       attacks
+    end
+
+    def diagonal_attacks
+      attacks = 0
+
+      queen_array = @queens.split(//).map { |i| Integer(i) }
+
+      queen_array.each_with_index do |queen, column|
+        for index in column..(queen_array.length - 1)
+          if (index != column) and ((queen == queen_array[index] + (index-column)) or (queen == queen_array[index] - (index-column)))
+            @log.debug "ADDING ATTACK on queen: #{queen} in #{column}\tpartner: #{queen_array[index]} in #{index}"
+            attacks += 1
+          end
+        end
+      end
+
+      attacks
+    end
+
+    def attacks
+      horizontal_attacks + diagonal_attacks
     end
 
     def valid_queen_positions(positions)
@@ -30,15 +57,22 @@ module EightQueens
       queen_array = @queens.split(//)
 
       pairs = []
-      queen_array.product(queen_array) do |first, second|
-        pairs << [first, second]
+
+      for i in 0..(queen_array.length-1)
+        for j in (i+1)..(queen_array.length-1)
+          pairs << [ queen_array[i], queen_array[j] ]
+        end
       end
 
-      diagonals = queen_array.zip(queen_array)
+      # queen_array.product(queen_array) do |first, second|
+      #   pairs << [first, second]
+      # end
 
-      diagonals.each do |d|
-        pairs.delete_at(pairs.index(d)) unless pairs.index(d).nil?
-      end
+      # diagonals = queen_array.zip(queen_array)
+
+      # diagonals.each do |d|
+      #   pairs.delete_at(pairs.index(d)) unless pairs.index(d).nil?
+      # end
 
       pairs
     end
--- spec/lib/state_spec.rb
+++ spec/lib/state_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 module EightQueens
   describe State do
 
-    let(:solution) { State.new("24613578") }
+    let(:solution) { State.new("24683175") }
 
     context "#initialize" do
 
-      it "should be instantiable" do 
+      it "should be instantiable" do
         s = State.new("12345678")
         s.should_not be nil
       end
@@ -20,15 +20,66 @@ module EightQueens
 
     end
 
-    context "#horizontal_attacks" do 
+    context "#horizontal_attacks" do
 
       it "should return 0 for a solution to the eight queens puzzle" do
         solution.horizontal_attacks.should eq 0
-      end 
+      end
 
       it "should return something else for a non-solution" do
         bad = State.new("22222222")
         bad.horizontal_attacks.should > 0
+      end
+
+      it "should return 1 for a solution with only one horizontally attacking pair" do
+        bad = State.new("22345678")
+        bad.horizontal_attacks.should eq 1
+      end
+    end
+
+    context "#diagonal_attacks" do
+
+      it "should return 0 for a solution to the eight queens puzzle" do
+        solution.diagonal_attacks.should eq 0
+      end
+
+      it "should return something else for a non-solution" do
+        bad = State.new("23456781")
+        bad.diagonal_attacks.should > 0
+      end
+    end
+
+    context "#attacks" do
+
+      it "should return 0 for a solution to the eight queens puzzle" do
+        solution.attacks.should eq 0
+      end
+
+      it "should return something else for a non-solution" do
+        bad = State.new("22222222")
+        other = State.new("23456781")
+        bad.attacks.should > 0
+        other.attacks.should > 0
+      end
+
+      it "should find 4 attacking pairs for 24748552" do
+        state = State.new("24748552")
+        state.attacks.should eq 4
+      end
+
+      it "should find 5 attacking pairs for 32752411" do
+        state = State.new("32752411")
+        state.attacks.should eq 5
+      end
+
+      it "should find 8 attacking pairs for 24415124" do
+        state = State.new("24415124")
+        state.attacks.should eq 8
+      end
+
+      it "should find 17 attacking pairs for 32543213" do
+        state = State.new("32543213")
+        state.attacks.should eq 17
       end
     end
 
